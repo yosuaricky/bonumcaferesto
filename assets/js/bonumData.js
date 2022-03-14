43ead9898c2309e1a6c6382e05d6056e0824a462
ysricky <yosuaricky2606@gmail.com>
2022-03-14 15:12:20 +0700

    change holiday notice date

--- assets/js/bonumData.js
+++ assets/js/bonumData.js
@@ -1,5 +1,5 @@
 const info =
-  "Dear customers, due to maintenance work, we are closed from 20 - 25 March 2022. We'll be open again on 26 March 2022. Thank you.";
+  "Dear customers, due to maintenance work, we are closed from 23 - 27 March 2022. We'll be open again on 28 March 2022. Thank you.";
 //'Please wash hand, wear mask, and maintain social distancing.';
 
 const menuPricelist = [
