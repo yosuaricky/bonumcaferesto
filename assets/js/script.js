ccb724120143bba057dbd17ff0b26c85dad535ea
yosuaricky <yosuaricky2606@gmail.com>
2023-09-08 21:11:04 +0700

    allow prettier to format template literal in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,30 @@ const accordion = document.querySelector('.accordion');
 menuPricelist.forEach((item, index) => {
   const accordionItem = document.createElement('div');
   accordionItem.classList.add('accordion-item');
-  accordionItem.innerHTML = `
-  <h2 class="accordion-header" id="flush-heading${index}">
-    <button
-      class="accordion-button collapsed"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#flush-collapse${index}"
-      aria-expanded="false"
-      aria-controls="flush-collapse${index}"
-    >
-      ${item.category}
-    </button>
-  </h2>
-  <div
-    id="flush-collapse${index}"
-    class="accordion-collapse collapse"
-    aria-labelledby="flush-heading${index}"
-    data-bs-parent="#accordionFlush"
-  >
-    <div class="accordion-body"><em>*click menu to show details</em></div>
-  </div>
-`;
+  accordionItem.innerHTML =
+    /* HTML */
+    `
+      <h2 class="accordion-header" id="flush-heading${index}">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flush-collapse${index}"
+          aria-expanded="false"
+          aria-controls="flush-collapse${index}"
+        >
+          ${item.category}
+        </button>
+      </h2>
+      <div
+        id="flush-collapse${index}"
+        class="accordion-collapse collapse"
+        aria-labelledby="flush-heading${index}"
+        data-bs-parent="#accordionFlush"
+      >
+        <div class="accordion-body"><em>*click menu to show details</em></div>
+      </div>
+    `;
   accordion.append(accordionItem);
 
   // generate accordion item
@@ -39,16 +41,18 @@ menuPricelist.forEach((item, index) => {
   item.menu.forEach((menuItem) => {
     const menuButton = document.createElement('div');
     menuButton.classList.add('menu-button');
-    menuButton.innerHTML = `
-    <button
-    class="btn btn-warning btn-sm"
-    type="button"
-    data-bs-toggle="modal"
-    data-bs-target="#modal-1"
-  >
-    ${menuItem.nama}
-  </button>
-    `;
+    menuButton.innerHTML =
+      /* HTML */
+      `
+        <button
+          class="btn btn-warning btn-sm"
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#modal-1"
+        >
+          ${menuItem.nama}
+        </button>
+      `;
 
     // menuButton handler to show modal
     menuButton.addEventListener('click', () => {
@@ -67,16 +71,18 @@ const cateringCard = document.querySelector('#cateringCard');
 menuCatering.forEach((item) => {
   const cateringItem = document.createElement('div');
   cateringItem.classList.add('col');
-  cateringItem.innerHTML = `<div class="card" style="width: 18rem">
-  <img src="${item.url}" class="card-img-top" alt="menu catering" />
-  <div class="card-body">
-    <h5 class="card-title">${item.nama}</h5>
-    <p class="card-text">
-      ${item.deskripsi}
-    </p>
-    <span class="badge bg-warning text-dark">${item.harga}</span>
-  </div>
-</div>`;
+  cateringItem.innerHTML =
+    /* HTML */
+    `
+      <div class="card" style="width: 18rem">
+        <img src="${item.url}" class="card-img-top" alt="menu catering" />
+        <div class="card-body">
+          <h5 class="card-title">${item.nama}</h5>
+          <p class="card-text">${item.deskripsi}</p>
+          <span class="badge bg-warning text-dark">${item.harga}</span>
+        </div>
+      </div>
+    `;
 
   cateringCard.append(cateringItem);
 });
